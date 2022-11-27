cb4c8e00017a1193ae8651f567e43f53584c35a3
Tolik <anatolijparygin988@gmail.com>
2022-11-27 21:51:54 +0300

    нибольшая цифра рандомного числа

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Clear();
+int nomberA = new Random ().Next(10,99);
+Console.WriteLine(nomberA);
+int a = nomberA/10;
+int b = nomberA%10;
+    if (a>=b) Console.Write(a);
+    else Console.Write(b);
