9b2666f397c6eb04be2e081b87cb5764e2c5692a
Tolik <anatolijparygin988@gmail.com>
2022-11-28 21:50:16 +0300

    является ли квадратлм?

---
+++ zadacha_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+Console.Clear();
+Console.WriteLine("Введите два числа: ");
+int firstnumber = int.Parse(Console.ReadLine());
+int secondnumber = int.Parse(Console.ReadLine());
+if (firstnumber * firstnumber == secondnumber || secondnumber * secondnumber == firstnumber)
+{
+    Console.Write($"{firstnumber}, {secondnumber} -> да");
+}
+else
+{
+    Console.Write ($" {firstnumber}, {secondnumber} -> нет");
+}
