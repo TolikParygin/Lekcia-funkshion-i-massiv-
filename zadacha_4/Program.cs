63625c90ca143a7c6b1b0846be25350a3c028681
Tolik <anatolijparygin988@gmail.com>
2022-11-28 21:20:19 +0300

    двойное сравнение

---
+++ zadacha_4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.Write($"{number} -> да");
+}
+else
+{
+    Console.Write($"{number} -> нет ");
+}
