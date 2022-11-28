00e7840263412883e4aba7125f0349710a3b2e63
Tolik <anatolijparygin988@gmail.com>
2022-11-28 20:04:44 +0300

    удаляет среднюю цифру из трехзначного числа

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+Console.Clear();
+int number_1 = new Random ().Next(100,1000);
+int nomber_2 = (number_1/100)*10 + number_1 % 10;
+Console.WriteLine (number_1 + " -> " + nomber_2);
