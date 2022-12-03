4b09b355453c253cd99eb3ebfa132fe4c344692b
Tolik <anatolijparygin988@gmail.com>
2022-12-04 00:13:24 +0300

    количество цифр в числе

---
+++ zadacha_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+int Figure(int N)
+{
+    int i = 0;
+    for (i = 0; N != 0; i++)
+    {
+        N = N / 10;
+    }
+    return i;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(number + " -> " + Figure(number));
