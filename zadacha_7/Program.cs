82e4fcfb05887531dcc9bca6a7fc11987864b897
Tolik <anatolijparygin988@gmail.com>
2022-12-03 23:04:08 +0300

    Сумма чисел от 1 до N

---
+++ zadacha_7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+Console.Clear();
+int Sum(int N)
+{
+    int result = 0;
+    for (int count = 1; count <= N; count++)
+    {
+        result = result + count;
+    }
+    return result;    
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = Sum (number);
+Console.Write(number + " -> " + result);
