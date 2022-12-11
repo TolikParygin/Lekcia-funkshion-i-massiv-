f5112e2b2a68d98562d5f578173e5653693cb5d2
Tolik <anatolijparygin988@gmail.com>
2022-12-11 12:54:19 +0300

    перевод из десятичной системы в двоичную

---
+++ zadacha_11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+void ToBinarySystem(int num)
+{
+    if(num <= 0)
+    {
+        return;
+    }
+    ToBinarySystem(num/2);
+    System.Console.Write(num%2);
+}
+
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+ToBinarySystem(number);
