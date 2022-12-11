f3029586ce3519b818cd40ebaf21f780dd14c665
Tolik <anatolijparygin988@gmail.com>
2022-12-11 12:45:44 +0300

    задача про треугольник

---
+++ zadacha_10/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// //Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Random rand = new Random();
+
+int numberA = rand.Next(1, 10);
+int numberB = rand.Next(1, 10);
+int numberC = rand.Next(1, 10);
+System.Console.WriteLine($"{numberA},{numberB}, {numberC}");
+
+bool Tringl =  false;
+
+if(numberA > numberB && numberA > numberC && numberA < numberB + numberC)
+    Tringl = true;
+if(numberB > numberA && numberB > numberC && numberB < numberA + numberC)
+    Tringl = true;
+if(numberC > numberB && numberC > numberA && numberC < numberB + numberA)
+    Tringl = true;
+
+if(Tringl)
+    System.Console.WriteLine($"Треугольник со сторонами {numberA}, {numberB}, {numberC} может быть");
+else
+    System.Console.WriteLine($"Треугольник со сторонами {numberA}, {numberB}, {numberC} не может быть");
