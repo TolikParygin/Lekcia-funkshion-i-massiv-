fcc4e5f1a04806d87799d838388422e0bf281248
Tolik <anatolijparygin988@gmail.com>
2022-12-11 13:17:15 +0300

    копия заданного массива

---
+++ zadacha_13/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+Console.Clear();
+System.Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // [-9; 10)
+    }
+    return arr;
+}
+
+
+int[] array = FillArrayWithRandomNumbers(N, 1, 10);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int[] array2 = new int [N];
+for (int i = 0; i < N; i++)
+{
+    array2[i] = array[i];
+}
+
+array2[1]=5;
+Console.WriteLine('[' + string.Join(", ", array2) + ']');
